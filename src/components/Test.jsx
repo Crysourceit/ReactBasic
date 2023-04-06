2e293bd1ea8c00c91a2143fc226958826d13ec6c
Crysource <hohomona21@gmail.com>
2023-04-06 14:38:14 +0900

    router

---
+++ src/components/Test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+const Test = () => {
+  return (
+    <div>
+      <Routes>Test</Routes>
+    </div>
+  );
+};
+
+export default Test;
